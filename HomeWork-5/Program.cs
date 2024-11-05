c8535c240adebf7915c1289acd9f0ebe16ef7742
CodeOtakuu <notbukaaa777@gmail.com>
2024-11-05 18:05:29 +0400

    Добавьте файлы проекта.

---
+++ HomeWork-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using University;
+
+Student Student_1 = new Student();
+Student Student_2 = new Student();
+
+try
+{
+    //normal data filling without getting exceptions
+    Student_1.Student_Name = "Hakob";
+	Student_1.Student_Age = 16;
+	Student_1.Student_Group = Student.Groupes.first;
+	Student_1.Student_University = "RAU University";
+	//incorrect data entry which will ultimately lead to an exception being thrown
+	//Student_2.Student_Name = "";
+	//Student_2.Student_Age = -10;
+	//Student_2.Student_Group = Student.Groupes.second;
+	//Student_2.Student_University = "RUK";
+
+    Student_1.ShowInfo(); //getting information about a student
+
+}
+catch (Exception ex)
+{
+	Console.WriteLine("incorrect data filling reason: {0}", ex.Message);
+}
+
---
+++ HomeWork-5/University/Student.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    internal class Student
+    {
+        public enum Groupes
+        {
+            first,
+            second,
+            third
+        }
+
+        private string Name;
+        private int Age;
+        private Groupes Group;
+        private string University;
+
+        private string Univ = "University";
+
+
+        public string Student_Name
+        {
+            get { return Name; }
+            set
+            {
+                if (value.Length > 0 && value.Length <= 50)
+                {
+                    Name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("The length of a student's name must be greater than zero and less than fifty");
+                }
+            }
+        }
+
+        public int Student_Age
+        {
+            get { return Age; }
+            set
+            {
+                if (value >= 10 && value <= 65)
+                {
+                    Age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("The student's age must be between ten and sixty-five");
+                }
+            }
+        }
+
+        public Groupes Student_Group
+        {
+            get { return Group; }
+            set { Group = value; }
+        }
+
+        public string Student_University
+        {
+            get { return University; }
+            set
+            {
+                if (value.Length > 0 && value.Length < 250 && value.Contains(Univ))
+                {
+                    University = value;
+                }
+                else
+                {
+                    throw new ArgumentException("The university name must be greater than zero and less than two hundred and fifty and must contain the word university.");
+                }
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: {0}", Name ?? "No Name");
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Group: {0}", Group);
+            Console.WriteLine("University: {0}", University ?? "No Data");
+        }
+
+
+    }
+}
